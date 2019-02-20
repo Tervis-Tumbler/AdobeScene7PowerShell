fd02a095419857d8071ff0ecb564c0f99e53cb97
Chris Magnuson <hourglasssand@gmail.com>
2019-02-20 12:18:22 -0500

    Add Out-AdobeScene7UrlPrettyPrint

--- AdobeScene7PowerShell.psm1
+++ AdobeScene7PowerShell.psm1
@@ -76,3 +76,43 @@ function ConvertTo-Scene7URLEncodedQueryStringParameterString {
         $URLEncodedQueryStringParameterString
     }
 }
+
+function Out-AdobeScene7UrlPrettyPrint {
+    param (
+        [Parameter(Mandatory,ValueFromPipeline)]$URL = "http://images.tervis.com/is/agm/tervis/6_cstm_print?&setAttr.imgWrap={source=@Embed(%27is(tervisRender/6oz_wrap_final%3flayer=1%26src=ir(tervisRender/6_Warp_trans%3f%26obj=group%26decal%26src=is(tervisRender/6oz_base2%3f.BG%26layer=5%26anchor=0,0%26src=is(tervis/prj-aa1f3d62-dd31-411e-bc46-b7c963e77ae0))%26show%26res=300%26req=object%26fmt=png-alpha,rgb)%26fmt=png-alpha,rgb)%27)}&setAttr.maskWrap={source=@Embed(%27http://images.tervis.com/is/image/tervis%3fsrc=(http://images.tervis.com/is/image/tervisRender/6oz_wrap_mask%3f%26layer=1%26mask=is(tervisRender%3f%26src=ir(tervisRender/6_Warp_trans%3f%26obj=group%26decal%26src=is(tervisRender/6oz_base2%3f.BG%26layer=5%26anchor=0,0%26src=is(tervis/prj-aa1f3d62-dd31-411e-bc46-b7c963e77ae0))%26show%26res=300%26req=object%26fmt=png-alpha)%26op_grow=-2)%26scl=1)%26scl=1%26fmt=png8%26quantize=adaptive,off,2,ffffff,00A99C%27)}&imageres=300&fmt=pdf,rgb&.v=72271&`$orderNum=11361062/2"
+    )
+    process {
+        $URLDecoded = $URL -replace "%26","&" -replace "%27","'" -replace "%3f","?"
+        $StringBuilder = New-Object -TypeName System.Text.StringBuilder
+        $CharactersToBreakAfter = "?"
+        $CharactersToBreakBefore = "&"
+        $CharactersToIndentOn = "{","("
+        $CharactersToUnindentOn = "}",")"
+        $IndentionLevel = 0
+    
+        foreach ($Character in $URLDecoded.ToCharArray()) {
+            if ($Character -in $CharactersToBreakAfter) {
+                [Void]$StringBuilder.Append("$Character`n")
+                [Void]$StringBuilder.Append("    " * $IndentionLevel)
+            } elseif ($Character -in $CharactersToBreakBefore) {
+                if ($StringBuilder[-1] -notin " ","`n") {
+                    [Void]$StringBuilder.Append("`n")
+                    [Void]$StringBuilder.Append("    " * $IndentionLevel)    
+                }
+                [Void]$StringBuilder.Append($Character)
+            } elseif ($Character -in $CharactersToIndentOn) {
+                [Void]$StringBuilder.Append("$Character`n")
+                $IndentionLevel += 1
+                [Void]$StringBuilder.Append("    " * $IndentionLevel)
+            } elseif ($Character -in $CharactersToUnindentOn) {
+                $IndentionLevel -= 1
+                [Void]$StringBuilder.Append("`n")
+                [Void]$StringBuilder.Append("    " * $IndentionLevel)
+                [Void]$StringBuilder.Append("$Character")
+            } else {
+                [Void]$StringBuilder.Append("$Character")
+            }
+        }
+        $StringBuilder.ToString()
+    }
+}
